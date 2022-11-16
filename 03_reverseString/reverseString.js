7d332ef714cb1c27d75467498531c1830f584a2e
HudaifDaif <daiftem@gmail.com>
2022-11-16 22:06:35 +0000

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function (str) {
+	let reversedString = "";
 
+	let i = str.length - 1;
+	while (i >= 0) {
+		reversedString += str[i];
+		i--;
+	}
+	return reversedString;
 };
 
 // Do not edit below this line
