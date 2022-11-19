081269cb0e084ba752934476b08537c04b62276e
HudaifDaif <daiftem@gmail.com>
2022-11-19 22:07:54 +0000

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (...theArgs) {
+	let targetArray = arguments[0];
+	let result = [];
+	for (let i = 1; i < theArgs.length; i++) {
+		let arg = theArgs[i];
 
+		for (let j = 0; j < targetArray.length; j++) {
+			if (targetArray[j] === arg) {
+				const x = targetArray.splice(j, 1);
+			}
+		}
+	}
+	return targetArray;
 };
 
 // Do not edit below this line
