439a57881266fe75a94a21bc963f0a062540ccde
HudaifDaif <daiftem@gmail.com>
2022-11-20 21:08:44 +0000

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,61 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (sumArray) {
+	let result = 0;
+	for (const n of sumArray) {
+		result += n;
+	}
+	return result;
 };
 
-const multiply = function() {
+const multiply = function (muliplyArray) {
+	let array = arguments[0];
 
+	if (array[0] === undefined) {
+		return 0;
+	}
+	let result = 1;
+	for (const n of muliplyArray) {
+		result = result * n;
+	}
+	return result;
 };
 
-const power = function() {
-	
+const power = function (num, index) {
+	let result = 1;
+	if (num == 0 || index == 0) {
+		return 0;
+	}
+	for (i = index; i > 0; i--) {
+		result *= num;
+	}
+	return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+	if (num === 0) {
+		return 1;
+	} else {
+		let result = 1;
+		for (let i = num; i > 0; i--) {
+			result *= i;
+		}
+		return result;
+	}
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
