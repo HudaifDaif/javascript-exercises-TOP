2a403dfe7b963e9896d1ecf7de431588657c049a
HudaifDaif <daiftem@gmail.com>
2022-11-20 13:49:34 +0000

    Complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,23 @@
-const ftoc = function() {
+const ftoc = function (degF) {
+	ftocConverted = (degF - 32) * (5 / 9);
 
+	if (ftocConverted % 1 === 0) {
+		return ftocConverted;
+	} else return parseFloat(ftocConverted.toFixed(1));
 };
 
-const ctof = function() {
+const ctof = function (degC) {
+	ctofConverted = (degC * 9) / 5 + 32;
 
+	if (ctofConverted % 1 === 0) {
+		return ctofConverted;
+	} else return parseFloat(ctofConverted.toFixed(1));
+
+	// (x * 9) / 5 + 32;
 };
 
 // Do not edit below this line
 module.exports = {
-  ftoc,
-  ctof
+	ftoc,
+	ctof,
 };
