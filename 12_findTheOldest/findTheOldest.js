0d892d9266c5d682eb260d98fcf5d6989051eace
HudaifDaif <daiftem@gmail.com>
2022-11-21 21:15:57 +0000

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,33 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+	let eldest = {
+		name: "",
+		yearOfBirth: 0,
+		yearOfDeath: 0,
+	};
+	for (person of people) {
+		if (person.yearOfDeath === undefined) {
+			person.yearOfDeath = new Date().getFullYear();
 
+			if (
+				person.yearOfDeath - person.yearOfBirth >
+				eldest.yearOfDeath - eldest.yearOfBirth
+			) {
+				eldest = person;
+			}
+		}
+
+		if (
+			person.yearOfDeath - person.yearOfBirth >
+			eldest.yearOfDeath - eldest.yearOfBirth
+		) {
+			eldest = person;
+		}
+	}
+
+	return eldest;
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+// ! Date().getFullYear()
