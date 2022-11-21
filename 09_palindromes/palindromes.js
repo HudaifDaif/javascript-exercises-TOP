4eebafa12f28b6ee7bb573ad01c394085e64ad0a
HudaifDaif <daiftem@gmail.com>
2022-11-21 07:08:15 +0000

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+	if (
+		str.replace(/[^a-zA-Z]/g, "").toLowerCase() ===
+		str
+			.replace(/[^a-zA-Z]/g, "")
+			.toLowerCase()
+			.split("")
+			.reverse()
+			.join("")
+	) {
+		return true;
+	} else {
+		return false;
+	}
 };
 
 // Do not edit below this line
