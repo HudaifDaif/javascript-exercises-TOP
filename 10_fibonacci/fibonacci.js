3c84c98f2b33568371ec731af79364db3f9ddfba
HudaifDaif <daiftem@gmail.com>
2022-11-21 20:15:21 +0000

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+	let a = 0;
+	let b = 1;
 
+	// num = parseInt(num);
+
+	if (num < 0) {
+		return "OOPS";
+	} else if (num == 1) {
+		return 1;
+	} else
+		for (i = 1; i < num; i++) {
+			result = a + b;
+			a = b;
+			b = result;
+		}
+	return result;
 };
 
 // Do not edit below this line
